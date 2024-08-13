b0ea6b882d40d3914b58872e3f23c172d7032e4b
saurabh050302 <saurabh050302@gmail.com>
2024-08-13 16:46:05 +0530

    toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import toast, { Toaster } from 'react-hot-toast';
+
 import deerLogo from './assets/deer.svg'
 import githubLogo from './assets/github.svg'
 import copyIcon from './assets/copy.svg'
@@ -8,64 +10,54 @@ import { generateMnemonic, mnemonicToSeedSync } from "bip39";
 import { derivePath } from "ed25519-hd-key";
 import { Keypair } from "@solana/web3.js";
 
-
 function App() {
 
   const [mnemonic, setMnemonic] = useState("");
   const [wallets, setWallets] = useState([]);
 
-  const [selectedCoin, setSelectedCoin] = useState(60);
-  const handleChange = (e) => {
-    setSelectedCoin(e.target.value);
-  };
-
   const handleGenerateMnemonic = () => {
     const mnemonic = generateMnemonic();
     setMnemonic(mnemonic);
     setWallets([]);
+    toast.success("Mnemonic Created!")
   }
 
   const handleGenerateWallet = () => {
-    if (mnemonic.split(" ").length != 12) throw new Error("Invalid Mnemonic")
-    const seed = mnemonicToSeedSync(mnemonic);
-    const path = `m/44'/${selectedCoin}'/${wallets.length}'/0'`;
-    const derivedSeed = derivePath(path, seed.toString("hex")).key;
-    const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
-    const kp = Keypair.fromSecretKey(secret);
-    const wallet = {
-      selectedCoin,
-      path,
-      publicKey: kp.publicKey.toBase58(),
-      secretKey: kp.secretKey.toString('hex')
+    try {
+      if (mnemonic.split(" ").length != 12) throw new Error("Invalid Mnemonic")
+      const seed = mnemonicToSeedSync(mnemonic);
+      const path = `m/44'/501'/${wallets.length}'/0'`;
+      const derivedSeed = derivePath(path, seed.toString("hex")).key;
+      const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+      const kp = Keypair.fromSecretKey(secret);
+      const wallet = {
+        path,
+        publicKey: kp.publicKey.toBase58(),
+        secretKey: kp.secretKey.toString('hex')
+      }
+      setWallets([...wallets, wallet])
+      toast.success("Wallet added!")
+    } catch (error) {
+      toast.error(error.message)
     }
-    setWallets([...wallets, wallet])
   }
 
   return (
-    <div className=' w-full min-h-screen bg-zinc-800 text-white p-10 flex flex-col gap-10'>
-      <div className='flex justify-between items-center'>
-        <div>
-          <a href="https://github.com/saurabh050302/web-wallet-" target='/'>
-            <img src={githubLogo} className=' w-12 h-12' />
-          </a>
-        </div>
+    <div className=' w-full min-h-screen bg-zinc-800 text-white p-10 flex flex-col items-center gap-10'>
+      <Toaster position='bottom-center' toastOptions={{ className: 'bg-zinc-700 text-white ' }} />
+      <div className='flex justify-between items-center w-3/4'>
         <div className=' flex justify-center items-center gap-4'>
           <img src={deerLogo} className=" w-14 h-14" />
           <h1 className='text-3xl font-semibold tracking-wide'>Web Wallet</h1>
         </div>
-        <select
-          value={selectedCoin}
-          onChange={handleChange}
-          className=' bg-zinc-700 p-2 rounded-md outline-none'>
-          <option value={60}>ETH</option>
-          <option value={501}>SOL</option>
-          <option value={0}>BTC</option>
-        </select>
+        <a href="https://github.com/saurabh050302/web-wallet-" target='/'>
+          <img src={githubLogo} className=' w-12 h-12' />
+        </a>
       </div>
 
-      <div className='flex flex-col justify-center items-center gap-4'>
+      <div className=' w-3/4 flex flex-col justify-center items-center gap-4'>
         <textarea
-          className={`h-10 w-3/4 bg-zinc-600 rounded-md outline-none px-2 py-1 resize-none text-xl text-center ${mnemonic.split(" ").length == 12 && "blur-sm"} hover:blur-none`}
+          className='h-10 w-3/4 bg-zinc-600 rounded-md outline-none px-2 py-1 resize-none text-xl text-center'
           placeholder='Paste or generate mnemonic'
           value={mnemonic}
           onChange={(e) => setMnemonic(e.target.value)}
@@ -77,21 +69,20 @@ function App() {
           >Generate Mnemonic</button>
           <button
             className=' bg-zinc-700 hover:bg-zinc-500 px-3 py-2 rounded-sm'
-            onClick={() => { navigator.clipboard.writeText(mnemonic) }}
+            onClick={() => { navigator.clipboard.writeText(mnemonic), toast.success("Mnemonic copied!") }}
           >Copy Mnemonic</button>
         </div>
       </div>
 
-      <div className='flex flex-col justify-center items-center gap-4'>
-        <h1 className=' text-3xl font-semibold tracking-wider'>Your {selectedCoin == 0 ? "BTC" : selectedCoin == 60 ? "ETH" : "SOL"} Wallets</h1>
+      <div className=' w-3/4 flex flex-col justify-center items-center gap-4'>
+        <h1 className=' text-3xl font-semibold tracking-wider'>Your Wallets</h1>
 
         {wallets.map((wallet, index) => (
-          wallet.selectedCoin == selectedCoin &&
           <div key={index} className='flex w-3/4 justify-between bg-zinc-600 gap-4 rounded-md'>
             <div className='flex'>
               <button
                 className=' px-3 py-2 bg-zinc-700 font-bold rounded-l-md'
-                onClick={() => { navigator.clipboard.writeText(wallet.publicKey) }}
+                onClick={() => { navigator.clipboard.writeText(wallet.publicKey), toast.success("Address copied!") }}
               >
                 <img src={copyIcon} className=' w-8 ' />
               </button>
